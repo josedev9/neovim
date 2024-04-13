03a249c237579c64ba2490b2a8dec22786c3ed8a
Jose Angel Rodriguez <joseangelrodrrodr@gmail.com>
2024-04-13 20:05:45 +0200

    Added colorizer config for nvim

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -110,6 +110,14 @@ return packer.startup(function(use)
         require('trouble').setup {}
     end
     }
+    -- Colorizer
+    use {
+    'NvChad/nvim-colorizer.lua',
+    config = function()
+        require('colorizer').setup {}
+    end
+
+    }
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
